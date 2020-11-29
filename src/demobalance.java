c1ef156ffe8ff8387069abb3cc5f2907b3886a01
Anastasia Baturina <89099934219@mail.ru>
2020-11-29 16:34:27 +0300

    initial commit

---
+++ src/demobalance.java
@@ -0,0 +1,15 @@
+public class demobalance {
+    public static void main(String[] args) {
+        int balance = 8900;
+        int bonus ;
+        if (balance > 1000) {
+            bonus = balance / 100;
+        } else {
+            bonus = 0;
+        }
+
+                System.out.println(bonus);
+
+
+    }
+}
